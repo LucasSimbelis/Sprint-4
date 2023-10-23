0d892781028b5276a6a6ece4614eba04116d499b
LucasSimbelis <lucas.simbelis.teixeira@gmail.com>
2023-10-23 20:45:19 -0300

    novo

---
+++ App.js
@@ -0,0 +1,18 @@
+import { NavigationContainer } from "@react-navigation/native";
+import Comanda from "./screens/Comanda";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Produtos from "./screens/Produtos";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown:false}}>
+        <Stack.Screen name="Produtos" component={Produtos} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+
+}
